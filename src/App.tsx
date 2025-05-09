09b196b30e9c3fbb02a9a13088cc489611546a7b
Bob Karemera Shema <bobshema14@gmail.com>
2025-05-09 17:06:24 +0200

    added sidebar with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import Navbar from './components/navbar/navbar.component';
+import Sidebar from './components/sidebar/sidebar.component';
 import MovieCard from './components/moviecard/moviecard.component';
-import { testData } from './assets/test-data';
+import { testData } from './assets/data/test-data';
 import './App.css';
 
 export default function App() {
   return (
     <>
       <Navbar />
-      {
-        testData.results.map((movie, index) => <MovieCard key={index} movie={movie} />)
-      }
+      <div className='page-body'>
+        <Sidebar />
+        <div className="movies-section">
+          <div className="movies-grid">
+            {
+              testData.results.map((movie) => <MovieCard key={movie.id} movie={movie} />)
+            }
+          </div>
+          <div className="pagination-container"></div>
+        </div>
+      </div>
     </>
   )
 }
