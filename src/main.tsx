f6944092b9b4dde4aa0250c34293a693c244d8e9
Bob Karemera Shema <bobshema14@gmail.com>
2025-06-01 14:40:02 +0200

    added state persistence on watchlist

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
-import { store } from './store/store.ts'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor, store } from './store/store.ts'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>,
 )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,14 +1,34 @@
-import { configureStore, type Action, type ThunkAction } from "@reduxjs/toolkit";
+import { combineReducers, configureStore, type Action, type ThunkAction } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 
 import moviesReducer from './moviesSlice';
 import watchlistReducer from './watchlistSlice';
 
-export const store = configureStore({
-    reducer: {
-        movies: moviesReducer,
-        watchlist: watchlistReducer
-    }
+const rootReducer = combineReducers({
+    movies: moviesReducer,
+    watchlist: watchlistReducer
 });
+
+// whitelist watchlist
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['watchlist']
+};
+
+// Persist reducers
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false
+    })
+});
+
+// Persist store
+export const persistor = persistStore(store);
 
 // Infer store type
 export type AppStore = typeof store;
